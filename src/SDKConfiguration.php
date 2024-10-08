e60889a87047712d08771a6a3a67ae3060e70c84
regression-test-executor <regression-test-executor@speakeasy.com>
2024-10-08 13:31:35 +0000

    openapi-generation@ea017e7f65a056b9e647185ff30146513580275d

--- src/SDKConfiguration.php
+++ src/SDKConfiguration.php
@@ -29,9 +29,9 @@ class SDKConfiguration
 
     public string $sdkVersion = '0.7.0';
 
-    public string $genVersion = '2.434.1';
+    public string $genVersion = '2.436.3';
 
-    public string $userAgent = 'speakeasy-sdk/php 0.7.0 2.434.1 0.0.1 dub/dub-php';
+    public string $userAgent = 'speakeasy-sdk/php 0.7.0 2.436.3 0.0.1 dub/dub-php';
 
     public function getServerUrl(): string
     {
